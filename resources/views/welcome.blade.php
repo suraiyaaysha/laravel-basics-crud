0222886ac68807acf525b44377c00f68aa1ae708
suraiyaaysha <suraiya@aminul.digital.com>
2024-08-21 16:02:00 +0600

    refactor: welcome file updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-{{-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -31,4 +31,4 @@
             </ul>
         </div>
     </body>
-</html> --}}
+</html>
